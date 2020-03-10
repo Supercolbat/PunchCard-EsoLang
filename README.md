0458ef107508d155ceb3f310244ddddb06d8cdd8
Supercolbat <supercolbat@gmail.com>
2020-03-10 00:14:08 +0000

    Added some commands

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # PunchCard Esoteric Language
-An esoteric programming language.
+An esoteric programming language that will be used in a punch card reader machine.
 
 I programmed this in [repl.it](https://repl.it) but if you want to use this on your machine, just copy `punchcard.java`
-
-[![Run on Repl.it](https://repl.it/badge/github/Supercolbat/PunchCard-PL)](https://repl.it/github/Supercolbat/PunchCard-PL)
 
 ## How it works
 There are only 8 "data spots" in each line.
 ---
 
 | Command     | What it does           |
-| --- | --- |
-|&nbsp;.&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;| push next line to cell |
-|&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;| pop cell               |
-|&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| ascii to chr           |
-|&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| ascii to dec           |
-|&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| add next line to cell  |
-|&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| sub next line to cell  |
-|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| output data in cell    |
-|&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| go back one cell       |
-|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..&nbsp;&nbsp;&nbsp;&nbsp;| go forward one cell    |
+| :-: | --- |
+|.&nbsp;&nbsp;.&nbsp;.&nbsp;.| push next line to cell |
+|&nbsp;&nbsp;..&nbsp;.&nbsp;.&nbsp;| pop cell |
+|&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| ascii to chr |
+|&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| ascii to dec           |
+|.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| sum next line to cell |
+|&nbsp;.&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;| sub next line to cell |
+|.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| output data in cell |
+|..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;| go back one cell |
+|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..| go forward one cell |
+
+[![Run on Repl.it](https://repl.it/badge/github/Supercolbat/PunchCard-PL)](https://repl.it/github/Supercolbat/PunchCard-PL)
--- punchcard.java
+++ punchcard.java
@@ -4,7 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         File f = new File();
-        System.out.println(f.readf(args));
+        Parser p = new Parser();
+        String arg;
+
+        List<String> flags = new ArrayList<String>();
+        int i = 0;
+        while (i < args.length && args[i].startsWith("-")) {
+            arg = args[i++];
+            if (arg.equals("-v") || arg.equals("--verbose")) {
+                flags.add("verbose");
+            } else {
+                System.out.println("Illegal option: " + arg);
+                System.exit(0);
+            }
+        }
+        if (flags.contains("verbose")) {
+            System.out.println(" [VERBOSE] File data: " + f.readf(args));
+        }
+        System.out.println(p.parse(f.readf(args)));
     }
 }
 
@@ -15,8 +32,8 @@ public class File
         ArrayList<String> lines = new ArrayList<>();
         String[] fileND = args[args.length-1].split("\\.");
 
-        if (args.length > 0) {
-            if (fileND[fileND.length-1].equals("pc")) {
+        if (fileND[fileND.length-1].equals("pc")) {
+            if (args.length > 0) {
                 try (Scanner s = new Scanner(new FileReader(String.join(".", fileND)))) {
                     while (s.hasNext()) {
                         lines.add(s.nextLine());
@@ -26,25 +43,64 @@ public class File
                     System.out.println(String.join(".", fileND) + " not found.");
                 }
             } else {
-                System.out.println("Incorrect file extension.");
+                System.out.println("Usage: punchcard [--verbose -v] filename");
                 System.exit(0);
             }
         } else {
-            System.out.println("Insufficient arguments.");
+            System.out.println("Illegal file extension.\nUsage: punchcard [--verbose -v] filename");
             System.exit(0);
         }
         return lines;
 	}
 }
 
-public class Lexer {
-    public void lex() {
-        //
-    }
-}
-
 public class Parser {
-    public void parse() {
-        //
+    public String parse(ArrayList<String> lines) {
+        boolean getc = true;
+        String run = "";
+        List<String> memory = new ArrayList<String>();
+        int pointer = 0;
+
+        for (String line : lines) {
+            line = line.replaceAll("[^.\\s]","");
+            if (getc) {
+                if (line.equals(".  . . .")) {
+                    getc = false;
+                    run = "push";
+                } else if (line.equals("  .. . . ")) {
+                    getc = false;
+                    run = "pop";
+                } else if (line.equals("..      ")) {
+                    getc = false;
+                    run = "bak";
+                } else if (line.equals("      ..")) {
+                    getc = false;
+                    run = "fwd";
+                }
+            } else {
+                if (run == "push") {
+                    String tmp = line.replace(".","1");
+                    tmp = tmp.replace(" ", "0");
+                    if (memory.size() == pointer) {
+                        memory.add(Integer.toString(Integer.parseInt(tmp,2)));
+                    } else {
+                        memory.set(pointer,Integer.toString(Integer.parseInt(tmp,2)));
+                    }
+
+                    getc = true;
+                    run = "";
+                } else if (run == "pop") {
+                    memory.set(pointer, "null");
+                    run = "";
+                } else if (run == "bak") {
+                    pointer++;
+                    run = "";
+                } else if (run == "fwd") {
+                    pointer--;
+                    run = "";
+                }
+            }
+        }
+        return memory.toString();
     }
 }
