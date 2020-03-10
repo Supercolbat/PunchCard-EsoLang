d7f79903e65d59405482565681f08646d29d7a48
Supercolbat <supercolbat@gmail.com>
2020-03-10 12:24:34 -0500

    Checkpoint
    I made a lot of progress on repl.it, but the site is having problems. I'm forced to program on the github site itself. :(
    F in the chat, gamers.
    
    The code might now work yet, I'm just worried something bad is going to happen and the code won't save.

--- punchcard.java
+++ punchcard.java
@@ -18,10 +18,13 @@ public class Main {
                 System.exit(0);
             }
         }
+		ArrayList<String> fdata = f.readf(args);
         if (flags.contains("verbose")) {
-            System.out.println(" [VERBOSE] File data: " + f.readf(args));
+			Parser.verbose("File data:"+fdata);
         }
-        System.out.println(p.parse(f.readf(args),flags));
+		for (String out: fdata) {
+			System.out.println(out);
+		}
     }
 }
 
@@ -55,56 +58,75 @@ public class File
 }
 
 public class Parser {
-    public String parse(ArrayList<String> lines, List<String> flags) {
+    public static String verbose(String s) { System.out.println(" [VERBOSE] "); }
+    public List<String> parse(ArrayList<String> lines, List<String> flags) {
         boolean getc = true;
         String run = "";
         List<String> memory = new ArrayList<String>();
         List<String> output = new ArrayList<String>();
         int pointer = 0;
+		boolean vFlag = flags.contains("verbose");
 
         for (String line : lines) {
-            if (line.length() >= 8) {
-                if (line.length() > 8) {
-                    line = line.replaceAll("[^.\\s]","").substring(0,8);
-                }
-                if (getc) {
-                    if (line.equals(".  . . .")) {
-                        getc = false;
-                        run = "push";
-                    } else if (line.equals(" .. . . ")) {
-                        memory.set(pointer, "");
-                        if (flags.contains("verbose")) {
-                            System.out.println(" [VERBOSE] cmd: pop");
-                        }
-                    } else if (line.equals("..      ")) {
-                        pointer--;
-                        if (flags.contains("verbose")) {
-                            System.out.println(" [VERBOSE] cmd: bak");
-                        }
-                    } else if (line.equals("      ..")) {
-                        pointer++;
-                        if (flags.contains("verbose")) {
-                            System.out.println(" [VERBOSE] cmd: fwd");
-                        }
-                    } else if (line.equals(" .      ")) {
-                        memory.set(0,Character.toString((char) Integer.parseInt(memory.get(pointer))));
-                    } else if (line.equals("  .     ")) {
-                        memory.set(0,Integer.toString(Integer.parseInt(memory.get(pointer))+48));
-                    } else if (line.equals(".  ..  .")) {
-                        memory.set(pointer,String.join("",memory));
-                    } else if (line.equals(".  ..  ..")) {
-                        memory.add("");
-                        memory.set(pointer,String.join("",memory.subList(pointer,memory.size())));
-                    } else if (line.equals("..  ..  .")) {
-                        if (pointer < memory.size()) {
-                            memory.set(pointer,String.join("",memory.subList(0,pointer+1)));
+			if (line.charAt(0) != ';') {
+                if (line.length() >= 8) {
+    	            if (line.length() > 8) {
+	                    line = line.replaceAll("[^.\\s]","").substring(0,8);
+	                }
+	                if (getc) {
+    	                if (line.equals(".  . . .")) {
+        	                getc = false;
+            	            run = "push";
+                	    } else if (line.equals(" .. . . ")) {
+                    	    memory.set(pointer, "");
+                        	if (vFlag) {
+								Parser.verbose("cmd: pop")
+    	                    }
+        	            } else if (line.equals("..      ")) {
+            	            pointer--;
+                	        if (vFlag) {
+								Parser.verbose("cmd: bak");
+								Parser.verbose("pointer: "+pointer");
+	                        }
+    	                } else if (line.equals("      ..")) {
+        	                pointer++;
+            	            if (vFlag) {
+                	            Parser.verbose("cmd: fwd");
+								Parser.verbose("pointer: "+pointer);
+                        	}
+	                    } else if (line.equals(" .      ")) {
+    	                    memory.set(0,Character.toString((char) Integer.parseInt(memory.get(pointer))));
+        	            } else if (line.equals("  .     ")) {
+            	            memory.set(0,Integer.toString(Integer.parseInt(memory.get(pointer))+48));
+                	    } else if (line.equals(".  ..  .")) {
+							String merged = String.join("",memory);
+							memory.clear();
+	                        memory.add(merged);
+    	                } else if (line.equals(".  ..  ..")) {
+							List<String> bef = new ArrayList<String>();
+        	                memory.add("");
+							bef.addAll(memory.subList(0,pointer-1));
+							bef.add(String.join("",memory.subList(pointer,memory.size()-1)));
+							memory.clear();
+							memory.addAll(bef);
+							memory.remove(memory.size()-1);
+                	    } else if (line.equals("..  ..  .")) {
+							memory.add("");
+							List<String> aft = memory.sublist(pointer+1,memory.size()-1);
+							aft.add(0,String.join("",memory.sublist(0,pointer+1)));
+							memory.clear();
+							memory.addAll(aft);
+							memory.remove(memory.size()-1);
                         } else {
                             memory.add("");
                             memory.set(pointer,String.join("",memory.subList(0,pointer+1)));
-                        }
-                    } else if (line.equals(".       ")) {
-                        output.add(memory.get(pointer));
-                    }
+	                    } else if (line.equals(".       ")) {
+    	                    output.add(memory.get(pointer));
+							if (vFlag) {
+								Parser.verbose("Pushed to memory: "+memory.get(pointer));
+							}
+							memory.set(pointer,"");
+        	            }
                 } else {
                     if (run == "push") {
                         String tmp = line.replace(".","1");
@@ -115,8 +137,8 @@ public class Parser {
                             memory.set(pointer,Integer.toString(Integer.parseInt(tmp,2)));
                         }
                         if (flags.contains("verbose")) {
-                            System.out.println(" [VERBOSE] cmd: push");
-                            System.out.println(" [VERBOSE] push: "+tmp);
+							Parser.verbose("cmd: push");
+							Parser.verbose("pushed: "+tmp);
                         }
                         
                         getc = true;
